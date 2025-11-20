f2587080609cbc00c61d49a376e3ff4ef325cf44
Caleb <ca30850@gmail.com>
2025-11-20 14:42:26 -0500

    create proyect and config initial

---
+++ src/main/java/com/franquicias/franquify/FranquifyApplication.java
@@ -0,0 +1,13 @@
+package com.franquicias.franquify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franquicias/franquify/FranquifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franquicias.franquify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
